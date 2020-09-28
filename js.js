8ad033ffea3cbecf95ec973bc1197d7a703f0dca
Tran Tuyet <trantuyet.fightforfreedom@gmail.com>
2020-09-28 23:35:49 +0700

    đã hoàn thiện các chức năng

--- js.js
+++ js.js
@@ -1,44 +1,70 @@
-class Mobile{
-    constructor(_name) {
+function Mobile(_name){
+
+        this.battery = 100;
         this.name = _name;
         this.msg = "";
         this.inbox = [];
         this.outbox = [];
+        this.status = false;
+
+        this.writeMsg = function (msg) {
+        if(this.battery > 0){
+            this.msg = msg;
+        }else
+            alert("Da het pin");
     }
 
-    writeMsg(msg){
-        this.msg = msg;
-    }
-    sendMsg(msg){
-            mobile.reiceiveMsg(this.name, this.msg);
-            this.outbox.push(this.msg);
+        this.sendMsg = function (mobile){
+            if(this.battery > 0){
+                mobile.receiveMsg(this.name,this.msg);
+                this.outbox.push(this.msg);
+                this.battery -= 10;
+            }else alert("Hết pin - Sập nguồn!");
         }
-    receiveMsg(name,msg) {
-        alert(name+ "has send a message");
-        this.inbox.push(msg);
-    }
+
+        this.receiveMsg = function (name,msg){
+            this.inbox.push(msg);
+        }
+
+        this.checkStatus = function () {
+            if(this.battery > 0){
+                return this.battery > 0}
+            else {alert("Đã hết pinnnn")}         ;
+            this.status = !this.status ;
+            }
+
+        this.chargeBattery = function () {
+            this.battery ++ ;
+        }
+
+
 }
-    let mobile1 = new Mobile("iphone");
-    let mobile2 = new Mobile("Nokia");
 
-    console.log(mobile1);
-    console.log(mobile2);
+let mobile1 = new Mobile("iphone");
+let mobile2 = new Mobile("nokia");
+    mobile1.chargeBattery();
+    mobile2.checkStatus();
 
-    let mess = prompt("Soạn tin nhắn");
+function writeMess(mobile,mess) {
+    mobile.writeMsg(mess);
+}
 
-    mobile1.writeMsg(mess);
+function sendMess(mobile1,mobile2) {
     mobile1.sendMsg(mobile2);
+    display();
+}
 
-    function writeMess(mobile, content) {
-        mobile.writeMesg(content)
-    }
-    function sendMess(mobile1, mobile2) {
-        mobile1.sendMsg(mobile2);
-    }
+function display() {
+    displayMobile(mobile1);
+    displayMobile(mobile2);
+}
 
-    function displayMobile(mobile) {
-        document.getElementById(mobile.name+ "-inbox").innerHTML = mobile.name ;
-        document.getElementById(mobile.name+ "-outbox").innerHTML = mobile.inbox ;
-        document.getElementById(mobile.name+ "-msg").value = "" ;
-    }
-    displayMobile();
+function displayMobile(mobile) {
+    document.getElementById(mobile.name+"-name").innerHTML = mobile.name;
+    document.getElementById(mobile.name+"-inbox").innerHTML = mobile.inbox;
+    document.getElementById(mobile.name+"-outbox").innerHTML = mobile.outbox;
+
+    document.getElementById(mobile.name+"-msg").value = "";
+}
+
+display();
